42ebbc2a7188579147e2d4f6d4910fceeab5e224
Dmitriy <43755002+psydvl@users.noreply.github.com>
2022-04-30 04:44:15 +0300

    Add update script

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -uex
+if [ $# == 0 ]
+then
+	array=(master ui)
+else
+	array=("$@")
+fi
+
+for BRANCH in "${array[@]}"; do
+	CHANGELOG="changelog"
+	if [ $BRANCH != "master" ]
+	then
+		CHANGELOG="$CHANGELOG-$BRANCH"
+	fi
+	git checkout $CHANGELOG
+	git branch -D $BRANCH
+	git switch --orphan $BRANCH
+	git commit --allow-empty -m "Init with gotk4 minimal template psydvl/gotk4-template"
+	git merge --squash --allow-unrelated-histories $CHANGELOG
+	git commit --amend --no-edit
+	git push -u origin $BRANCH --force-with-lease
+done
